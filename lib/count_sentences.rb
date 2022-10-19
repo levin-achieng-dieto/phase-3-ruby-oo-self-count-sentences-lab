ba55ab392b2dd32555ea326c7587ea871979635a
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-10-19 14:26:05 +0300

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 class String
 
+  # attr_accessor :str
+  # def initialize(str)
+  #   @str = str
+  # end
+
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence = self.split(/[\.\?\!]/)
+    count = sentence.reject { |e| e == ""}
+    count.size
   end
 end
